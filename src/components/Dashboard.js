6d04ae88be5994c5ef700a6aebc2351eba0efa81
Mattia Lavai <mattialavai@gmail.com>
2024-07-10 20:43:23 +0000

    dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,9 +6,11 @@ import todoImage from '../images/undraw_mobile_123.png'; // Import the image
 const Dashboard = () => (
   <div className="dashboard-container">
     <div className="header-section">
-      <img src={todoImage} alt="Get Things Done" />
       <h2>Get Things Done with Todo</h2>
       <p>Start organizing your tasks efficiently with our simple and intuitive todo app.</p>
+    </div>
+    <div className="image-section">
+      <img src={todoImage} alt="Get Things Done" />
     </div>
     <div className="button-section">
       <Link to="/signup" className="get-started-button">Get Started</Link>
